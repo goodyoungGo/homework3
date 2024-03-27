41e56de4fea90c124c6356e37d3565516ffa00f8
goodyoung <goodyoung0615@gmail.com>
2024-03-27 10:09:48 +0900

    Update: change comment

--- p2-1.c
+++ p2-1.c
@@ -41,32 +41,32 @@ void main(void) {
 
 float sum1(float list[], int n) {
     printf("list \t= %p\n", list); // 매개변수 list 배열이 저장된 시작 주소 출력
-    printf("&list \t= %p\n\n", &list); // 매개변수 list 배열이 저장되어있는 있는 주소 출력
+    printf("&list \t= %p\n\n", &list); // 매개변수  배열이 저장되어있는 있는 주소 출력
     int i; // 정수형 변수 i 선언
     float tempsum = 0; // 실수형 변수 tempsum에 0 값 할당.
     for(i = 0; i < n; i++) // i가 0에서 99까지 100번 반복 (매개변수 정수형 변수 n은 100의 값을 받았다.)
-        tempsum += list[i]; // 매개변수 list 배열의 모든 요소를 누적하여 실수형 변수 tempsum에 더함.
+        tempsum += list[i]; // 매개변수 list 인덱스에 접근을 하여 값을 얻는 방식으로 모든 요소를 누적하여 실수형 변수 tempsum에 더함.
     return tempsum; // 실수형 변수 tempsum을 반환.
 }
 
 float sum2(float *list, int n) {
     printf("list \t= %p\n", list); // 매개변수 list 배열이 저장된 시작 주소 출력
-    printf("&list \t= %p\n\n", &list); // 매개변수 list 배열이 저장되어있는 있는 주소 출력
+    printf("&list \t= %p\n\n", &list); // 매개변수  배열이 저장되어있는 있는 주소 출력
     int i; // 정수형 변수 i 선언
     float tempsum = 0; // 실수형 변수 tempsum에 0 값 할당.
 
     for(i = 0; i < n; i++) // i가 0에서 99까지 100번 반복 (매개변수 정수형 변수 n은 100의 값을 받았다.)
-        tempsum += *(list + i); // 매개변수 list 배열의 모든 요소를 누적하여 실수형 변수 tempsum에 더함.
+        tempsum += *(list + i); // 매개변수 list 배열의 i번째 공간 안의 값에 접근하는 방식으로 모든 요소를 누적하여 실수형 변수 tempsum에 더함.
     return tempsum; // 실수형 변수 tempsum을 반환.
 }
 
 /* stack variable reuse test */
 float sum3(int n, float *list) {
     printf("list \t= %p\n", list); // 매개변수 list 배열이 저장된 시작 주소 출력
-    printf("&list \t= %p\n\n", &list); // 매개변수 list 배열이 저장되어있는 있는 주소 출력
+    printf("&list \t= %p\n\n", &list); // 매개변수  배열이 저장되어있는 있는 주소 출력
     int i; // 정수형 변수 i 선언
     float tempsum = 0; // 실수형 변수 tempsum에 0 값 할당.
     for(i = 0; i < n; i++) // i가 0에서 99까지 100번 반복 (매개변수 정수형 변수 n은 100의 값을 받았다.)
-        tempsum += *(list + i); // 매개변수 list 배열의 모든 요소를 누적하여 실수형 변수 tempsum에 더함.
+        tempsum += *(list + i); // 매개변수 list 배열의 i번째 공간의 주소내의 값에 접근하는 방식으로 모든 요소를 누적하여 실수형 변수 tempsum에 더함.
     return tempsum; // 실수형 변수 tempsum을 반환.
 }
